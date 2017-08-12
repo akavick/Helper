1444464667ccfca68b452183053f5ee94e6e71aa
akavick <akavick@tut.by>
2017-08-13 00:54:55 +0300

    1

--- HelperSolution/MainConsoleTestProject/Program.cs
+++ HelperSolution/MainConsoleTestProject/Program.cs
@@ -9,8 +9,8 @@ namespace MainConsoleTestProject
             //new MatrixPainter().Run();
             //new ChainFunction().Run();
             //new StaticGcTest().Run();
-            new ProcessTest().Run();
-
+            //new ProcessTest().Run();
+            new Tricks().Run();
 
             Console.ReadKey(true);
         }
---
+++ HelperSolution/MainConsoleTestProject/Tricks.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExtensionsNs
+{
+    public static class Extensions
+    {
+        public static TOut With<TIn, TOut>(this TIn self, Func<TIn, TOut> func) 
+            where TIn : class 
+            where TOut : class
+        {
+            return self == null ? null : func(self);
+        }
+    }
+}
+
+
+namespace MainConsoleTestProject
+{
+    using ExtensionsNs;
+    class A
+    {
+        public B B { get; set; }
+    }
+
+    class B
+    {
+        public C C { get; set; }
+    }
+
+    class C
+    {
+        public object O { get; set; }
+    }
+    public class Tricks
+    {
+
+        public void Run()
+        {
+            var a = new A {B = new B()};
+            var r = a
+                .With(x => x.B)
+                .With(x => x.C)
+                .With(x => x.O)
+                .With(x => x.GetType());
+            
+            Console.WriteLine(r);
+        }
+    }
+}
