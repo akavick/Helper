4f6221517e29b5a12b4ed960e4028d334c0e96c6
akavick <akavick@tut.by>
2017-09-16 00:42:25 +0300

    1

--- HelperSolution/MainConsoleTestProject/Program.cs
+++ HelperSolution/MainConsoleTestProject/Program.cs
@@ -54,14 +54,70 @@ namespace MainConsoleTestProject
 
     }
 
+
+    internal class OneAtATimePlease
+    {
+        public void Run()
+        {
+            // Назначение объекту Mutex имени делает его доступным на уровне всего компьютера.
+            // Используйте имя, являющееся уникальным для вашей компании и приложения
+            // (например, включите в него URL компании) .
+            using (var mutex = new Mutex(true, "VeryLongAndUniqueMutexName"))
+            {
+                // Ожидать несколько секунд, если возникло соперничество; в этом случае
+                // другой экземпляр прог раммы все еше находится в процессе завершения.
+                if (!mutex.WaitOne(TimeSpan.FromSeconds(3), false))
+                {
+                    Console.WriteLine("Another iпstance of the арр is running. Буе!");
+                    // Выполняется другой экземпляр прог раммы
+                    return;
+                }
+                try
+                {
+                    RunProgram();
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+
+        static void RunProgram()
+        {
+            Console.WriteLine("Runпiпg. Press Enter to exit");
+            // Программа выполняется; нажмите Enter для завершения
+            Console.ReadLine();
+        }
+    }
+
+
+
+
+
+
     internal class Program
     {
         private static void Main(string[] args)
         {
-            new Program().Run15();
+            new OneAtATimePlease().Run();
+            //new Program().Run15();
             Console.ReadKey(true);
         }
 
+
+        private void StringByHalfReplase()
+        {
+            while (true)
+            {
+                var s = Console.ReadLine();
+                //var result = s?.Substring(s.Length % 2 == 0 ? s.Length / 2 : s.Length / 2 + 1) + s?.Substring(0, s.Length % 2 == 0 ? s.Length / 2 : s.Length / 2 + 1);
+
+                var result = new string(s?.Where((c, i) => i % 3 != 0).ToArray());
+
+                Console.WriteLine(result);
+            }
+        }
 
 
 
