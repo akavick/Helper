19998d61fbb55dc0cacad4cbf664382eee9a1dd8
akavick <akavick@tut.by>
2018-03-01 13:38:34 +0300

    1

---
+++ HelperSolution/ReflTest001/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflTest001
+{
+
+    internal class MyClass
+    {
+        public int Prop { get; set; }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var cl = new MyClass();
+            var obj = new object();
+            obj = new ObjectDisposedException("hi");
+
+            Console.WriteLine(obj.GetType().Name);
+
+
+        }
+    }
+}
