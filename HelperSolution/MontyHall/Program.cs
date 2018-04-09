e731be49bb1b0503ac8055c3fe9575030d26a1a6
akavick <akavick@tut.by>
2018-04-10 02:00:26 +0300

    1

--- HelperSolution/MontyHall/Program.cs
+++ HelperSolution/MontyHall/Program.cs
@@ -3,42 +3,46 @@ using System.Linq;
 
 namespace MontyHall
 {
-    internal class Door
-    {
-        public bool Value { get; set; }
-        public bool Opened { get; set; }
-    }
-
     internal class Program
     {
         private static void Main(string[] args)
         {
             const int iterations = 10000;
             var r = new Random();
-            var result = Enumerable.Range(1, iterations)
-                                   .Select(i =>
-                                   {
-                                       const int cnt = 1000;
-                                       var doors = Enumerable.Range(0, cnt)
-                                                             .Select(index => new Door())
-                                                             .ToList();
-                                       doors[r.Next(cnt)].Value = true;
-                                       var choice = doors[r.Next(cnt)];
-                                       doors.Where(d => d.Value == false && d != choice)
-                                            .ToList()
-                                            .ForEach(d => d.Opened = true);
-                                       if (choice.Value)
-                                       {
-                                           doors.Where(d => d.Value == false)
-                                                .ElementAt(r.Next(cnt - 1)).Opened = false;
-                                       }
+            var result1 = Enumerable
+                         .Range(1, iterations)
+                         .Select(i =>
+                         {
+                             const int cnt = 1000;
+                             var prizeDoorNumber = r.Next(cnt);
+                             var selectedDoorNumber = r.Next(cnt);
+                             var doors = Enumerable
+                                         .Range(0, cnt)
+                                         .Select(index => (Value: index == prizeDoorNumber, Opened: false, Number: index))
+                                         .ToArray();
+                             var closedUnselectedDoors = doors.Where(door => door.Value == false && door.Number != doors[selectedDoorNumber].Number);
 
-                                       var c = choice;
-                                       choice = doors.Single(d => d.Opened == false && d != c);
-                                       return choice.Value;
-                                   })
-                                   .Count(val => val) * 1.0m / iterations;
-            Console.WriteLine(result * 100);
+                             foreach (var door in closedUnselectedDoors)
+                             {
+                                 doors[door.Number].Opened = true;
+                             }
+
+                             if (doors[selectedDoorNumber].Value)
+                             {
+                                 var loseDoorsNumbers = doors.Where(d => d.Value == false)
+                                                             .Select(d => d.Number)
+                                                             .ToArray();
+
+                                 doors[r.Next(loseDoorsNumbers.Length)].Opened = false;
+                             }
+
+                             var choice = doors.Single(door => door.Opened == false && door.Number != doors[selectedDoorNumber].Number);
+
+                             return choice.Value;
+                         })
+                         .Count(val => val) * 1.0m / iterations;
+
+            Console.WriteLine(result1 * 100);
         }
     }
 }
