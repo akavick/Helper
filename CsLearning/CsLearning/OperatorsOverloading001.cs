329e64affc3a91423a60a61430f2f6d2a61ea98d
akavick <akavick@tut.by>
2018-09-12 16:22:03 +0300

    1

---
+++ CsLearning/CsLearning/OperatorsOverloading001.cs
@@ -0,0 +1,24 @@
+﻿namespace CsLearning
+{
+    public class OperatorsOverloading001 : ILesson
+    {
+        
+
+
+
+
+        public void Run()
+        {
+            System.Console.WriteLine(GetType());
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ CsLearning/CsLearning/Structs001 - Копировать.cs
@@ -0,0 +1,24 @@
+﻿namespace CsLearning
+{
+    public class Structs001 : ILesson
+    {
+        
+
+
+
+
+        public void Run()
+        {
+            System.Console.WriteLine(GetType());
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
