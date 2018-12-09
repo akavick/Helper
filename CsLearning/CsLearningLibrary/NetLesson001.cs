9d4333543a9b2aa15b19f83b3e223107ee3a77a8
akavick <akavick@tut.by>
2018-12-10 00:25:41 +0300

    1

--- CsLearning/CsLearningLibrary/NetLesson001.cs
+++ CsLearning/CsLearningLibrary/NetLesson001.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 
@@ -20,6 +22,27 @@ namespace CsLearningLibrary
             Console.WriteLine($"Host: {uri.Host}");
             Console.WriteLine($"Path: {uri.AbsolutePath}");
             Console.WriteLine($"Query: {uri.Query}");
+
+
+            IPHostEntry entry = Dns.GetHostEntry(uri.Host);
+            Console.WriteLine($"{entry.HostName} has the following IP addresses:");
+
+            foreach (IPAddress address in entry.AddressList)
+            {
+                Console.WriteLine($" {address}");
+            }
+
+
+            var ping = new Ping();
+            PingReply reply = ping.Send(uri.Host);
+
+            Console.WriteLine($"{uri.Host} was pinged, and replied: {reply?.Status}."); //timeout - protection from DDoS
+
+            if (reply?.Status == IPStatus.Success)
+            {
+                Console.WriteLine($"Reply from {reply.Address} took{ reply.RoundtripTime:N0}ms");
+            }
+
         }
     }
 
