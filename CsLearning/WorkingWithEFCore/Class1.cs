dfcc70c5cef92ef67725fcce8d70f5cdc7a31d2e
akavick <akavick@tut.by>
2019-02-08 23:42:42 +0300

    1

---
+++ CsLearning/WorkingWithEFCore/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace WorkingWithEFCore
+{
+
+    // управление соединением с базой данных
+    public class NorthwindDbContext: DbContext
+    {
+        // свойства, сопоставляемые с таблицами в базе данных
+        //public DbSet<Category> Categories { get; set; }
+
+        //public DbSet<Product> Products { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // для Microsoft SQL Server
+            optionsBuilder.UseSqlServer(
+            @"Data Source=.;" +
+            "Initial Catalog=Northwind;" +
+            "Integrated Security=true;" +
+            "MultipleActiveResultSets=true;");
+        }
+    }
+
+}
